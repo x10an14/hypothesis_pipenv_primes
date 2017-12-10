1b6172fa4740cfb0135ed0edd59f577505abe34e
x10an14 <x10an14@hotmail.com>
2017-12-10 14:13:06 +0100

    Minor update

--- tests/test_sieve.py
+++ tests/test_sieve.py
@@ -4,17 +4,19 @@ from prime import sieve
 from hypothesis import given
 from hypothesis import settings
 from hypothesis import Verbosity
+from hypothesis.strategies import values
 from hypothesis.strategies import sampled_from
 from hypothesis.strategies import integers as ints
 
 
 @given(
     ints(max_value=max(FIRST_MILLION_PRIMES)) |
-    sampled_from(FIRST_MILLION_PRIMES)
+    sampled_from(FIRST_MILLION_PRIMES) |
+    values()
 )
 @settings(
     verbosity=Verbosity.verbose,
-    max_examples=1_000
+    max_examples=1_000,
 )
 def test_prime_sieve(pot_prime):
     try:
